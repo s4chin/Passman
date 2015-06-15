66024cdf0b992231b507fe69963e009e9f1c0287
Sachin Kelkar <sachinkel19@gmail.com>
2015-06-16 01:08:55 +0530

    Ready base

--- Passman.py
+++ Passman.py
@@ -2,6 +2,10 @@ from hashlib import sha256
 
 SECRET_KEY = 's3cr3t'
 
+ALPHABET = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    'abcdefghijklmnopqrstuvwxyz'
+    '0123456789!@#$%^&*()-_')
+
 def get_hexdigest(text1, text2):
     return sha256(text1 + text2).hexdigest()
 
@@ -10,5 +14,20 @@ def make_password(plaintext, service):
     hsh = get_hexdigest(salt, service)
     return ''.join(salt + hsh)
 
+def password(plaintext, service, length=16, alphabet=ALPHABET):
+    word = make_password(plaintext, service)
+
+    num = int(word, 16)
+
+    num_chars = len(alphabet)
+
+    chars = []
+
+    while(len(chars)<length):
+        num, idx = divmod(num, num_chars)
+        chars.append(alphabet[num_chars-idx])
+
+    return ''.join(chars)
+
 if __name__ == '__main__':
-    print make_password('password', 'facebook')
+    print password('password', 'facebook')
